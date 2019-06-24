a60317631b61deae4375e7fe4367949b1de89a4d
Ben Hutchison <ben@aldaviva.com>
2019-06-24 05:23:47 -0700

    Added Gradle, settings may not be fully synchronized with IntelliJ yet

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+group = "com.aldaviva.midi"
+version = "0.0.1-SNAPSHOT"
+
+plugins {
+    `maven-publish`
+    kotlin("jvm") version "1.3.40"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+//            artifact(dokkaJar)
+        }
+    }
+    repositories {
+        maven {
+            url = uri("$buildDir/repository")
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "behringer-xtouch-extender"
